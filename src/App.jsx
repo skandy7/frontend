9cfe3d7ac375bfcde299d58b7c27f9696d64cd1f
skandy777 <huskanda7@gmail.com>
2025-06-11 16:15:13 +0530

    Fix: Firebase setup and AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvide } from './context/AuthContext'
 
 function App() {
 
   return (
     <>
-      <AuthProvider>
+      <AuthProvide>
 
       <Navbar/>
       <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py -6 font-primary'>
@@ -17,7 +17,7 @@ function App() {
       </main>
       <Footer/>
         
-      </AuthProvider>
+      </AuthProvide>
      
     </>
   )
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,7 +11,7 @@ export const useAuth = () => {
 const googleProvider = new GoogleAuthProvider();
 
 // authProvider
-export const AuthProvider = ({children}) => {
+export const AuthProvide = ({children}) => {
     const [currentUser, setCurrentUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
